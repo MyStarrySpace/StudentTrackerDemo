a83678a0ef0fe4241477a5103962af82604fb729
Shirley Xu <shirleyxu@umass.edu>
2019-02-25 16:43:06 -0500

    balivuarw

--- TodoApp/todo.js
+++ TodoApp/todo.js
@@ -1,1 +1,9 @@
+function addItemToList() {
+    var item = document.forms["addItem"]["newItem"].value;
+    if (item == "") {
+      alert("You forgot to type an item in!");
+    }
+    else{
 
+    }
+  }

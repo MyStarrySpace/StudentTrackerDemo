bac489b0140e53eafe92ec194ad390eccc16e7ed
Shirley Xu <shirleyxu@umass.edu>
2019-04-07 17:29:18 -0400

    Update server.js

--- project-template/server.js
+++ project-template/server.js
@@ -7,46 +7,42 @@ const app = express();
 app.use(express.static('static'));
 app.use(bodyParser.json());
 
-app.listen(3000, function () {
-    console.log('App started on port 3000');
-});
-
 const MongoClient = require('mongodb').MongoClient;
 
 const studentFieldType = {
   name: 'required',
+  major: 'required',
   bio: 'required',
-  courses: 'required',
+  courses: 'optional',
 };
 
-// app.get('/api/students', (req, res) => {
-//   const filter = {};
-//   if (req.query.status) filter.status = req.query.status;
+app.get('/api/students', (req, res) => {
+  const filter = {};
+  if (req.query.major) filter.major = req.query.major;
 
-//   db.collection('students').find(filter).toArray().then(students => {
-//     const metadata = { total_count: students.length };
-//     res.json({ _metadata: metadata, records: students })
-//   }).catch(error => {
-//     console.log(error);
-//     res.status(500).json({ message: `Internal Server Error: ${error}` });
-//   });
-// });
+  db.collection('students').find(filter).toArray().then(students => {
+    const metadata = { total_count: students.length };
+    res.json({ _metadata: metadata, records: students })
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json({ message: `Internal Server Error: ${error}` });
+  });
+});
 
-// app.post('/api/students', (req, res) => {
-//   const newStudent = req.body;
-//   newStudent.created = new Date();
-//   if (!newStudent.status)
-//     newStudent.status = 'New';
+app.post('/api/students', (req, res) => {
+  const newStudent = req.body;
+   if (!newStudent.courses)
+    newStudent.courses = {};
 
-//   db.collection('students').insertOne(newStudent).then(result =>
-//     db.collection('students').find({ _id: result.insertedId }).limit(1).next()
-//   ).then(newStudent => {
-//     res.json(newStudent);
-//   }).catch(error => {
-//     console.log(error);
-//     res.status(500).json({ message: `Internal Server Error: ${error}` });
-//   });
-// });
+  db.collection('students').insertOne(newStudent).then(result =>
+    db.collection('students').find({ _id: result.insertedId }).limit(1).next()
+  ).then(newStudent => {
+    res.json(newStudent);
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json({ message: `Internal Server Error: ${error}` });
+  });
+});
 
 let db;
 MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(connection => {
